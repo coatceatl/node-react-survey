2d69916c4a4054d32a07d515a8d99efc7d6e2492
Oksana Tkachenko <oksana@crystax.net>
2018-05-07 22:15:59 +0300

    adds static file(index.html) to server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('server node/express');
-});
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 5000;
 
